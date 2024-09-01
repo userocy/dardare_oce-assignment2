faa1194dbd93c439caed342aea1c43a5a1a154ac
Oceane <dardareoceane@gmail.com>
2024-09-02 01:37:16 +0800

    New commit: Added User Age Input

--- main.c
+++ main.c
@@ -2,12 +2,15 @@
 
 int main() {
     char user[10];
+    int age;
     
     printf("Hello World!\n");
     printf("Enter name: ");
     scanf("%s", user);
+    printf("Enter age: ");
+    scanf("%d", &age);
     
-    printf("Hello, %s!", user);
+    printf("Hello, %s! You're %d years old!", user, age);
 
     return 0;
 }
