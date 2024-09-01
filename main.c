56a1aec1b02cb711d8c953ab07fb1aba71134a15
Oceane <dardareoceane@gmail.com>
2024-09-02 01:12:08 +0800

    Add personalized greeting feature

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 
 int main() {
-    printf("Hello World!");
+    char user[10];
+    
+    printf("Hello World!\n");
+    printf("Enter name: ");
+    scanf("%s", user);
+    
+    printf("Hello, %s!", user);
+
     return 0;
 }
