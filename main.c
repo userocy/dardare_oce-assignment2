61a4631578828732c19ada2b98e67cbfa663aa6f
Jay Abordo <francizjay07.fja@gmail.com>
2024-09-03 17:27:51 +0800

    i increased the array size of coureyear

--- main.c
+++ main.c
@@ -1,17 +1,19 @@
 #include<stdio.h>
 
-int main() {
-    char user[10], courseyr[5];
+int main(void) {
+    char user[10], courseyr[10];
     int age;
     
     printf("Hello World!\n");
     printf("Enter name: ");
     scanf("%s", user);
+
     printf("Enter age: ");
     scanf("%d", &age);
+
     printf("Enter course and year: ");
     scanf("%s", courseyr);
-    
+
     printf("Hello, %s!\nYou're %d years old in %s!", user, age, courseyr);
 
     return 0;
